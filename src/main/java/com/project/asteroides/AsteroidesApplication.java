f91710f2e609aa3f404ce84494d74d2a9e9471e0
Mario23junior <mariojunior3251@gmail.com>
2021-05-14 05:01:19 -0300

    Iniciando projeto de api de asteroides e configurando travis-ci

---
+++ src/main/java/com/project/asteroides/AsteroidesApplication.java
@@ -0,0 +1,13 @@
+package com.project.asteroides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/asteroides/AsteroidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.asteroides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
