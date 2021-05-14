99dca403cc324f751d6c341103cbdfa70dd79d5f
Mario23junior <mariojunior3251@gmail.com>
2021-05-14 06:37:11 -0300

    Criando camada de repository da entidade DetalhesData

---
+++ src/main/java/com/project/asteroides/Repository/RepositoryDetalhesDatas.java
@@ -0,0 +1,9 @@
+package com.project.asteroides.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.asteroides.Model.DetalhesDatas;
+
+public interface RepositoryDetalhesDatas extends JpaRepository<DetalhesDatas, Long>{
+
+}
