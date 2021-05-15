4a41e3e495fa765c2d7b4e00e103450e8e094599
Mario23junior <mariojunior3251@gmail.com>
2021-05-15 11:41:43 -0300

    Salvando as definicaoes cientificas dos asteroides na base de dados

--- src/main/java/com/project/asteroides/Controller/ControllerDefinicaoCientifica.java
+++ src/main/java/com/project/asteroides/Controller/ControllerDefinicaoCientifica.java
@@ -1,8 +1,11 @@
 package com.project.asteroides.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.asteroides.Model.DefinicaoCientifica;
 import com.project.asteroides.Controller.Service.ServiceDefinicaoCientifica;
 
 @RequestMapping("/api/v1/definicaoCientifica")
@@ -15,5 +18,8 @@ public class ControllerDefinicaoCientifica {
 		this.service = service;
  	}
 	
-	
+	@PostMapping("/")
+	public DefinicaoCientifica definicaoCientificaSave(@RequestBody DefinicaoCientifica definicaoCientifica) {
+		return service.SaveDefinicaoCientifica(definicaoCientifica);
+ 	}
 }
