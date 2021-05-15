66d71801eba66c96cc9c003f05448ee705d91e44
Mario23junior <mariojunior3251@gmail.com>
2021-05-15 09:06:15 -0300

    Deletando informacaoes de asteroides por id

--- src/main/java/com/project/asteroides/Controller/ControllerAsteroides.java
+++ src/main/java/com/project/asteroides/Controller/ControllerAsteroides.java
@@ -2,12 +2,15 @@ package com.project.asteroides.Controller;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.asteroides.Model.Asteroide;
 import com.project.asteroides.Repository.RepositoryAsteroides;
@@ -33,4 +36,14 @@ public class ControllerAsteroides {
 	    Optional<Asteroide> list = repositoryAsteroides.findById(id);
 	    return list;
 	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		 repositoryAsteroides 
+		           .findById(id)
+		           .map(AnonimeDelete -> {
+		        	   repositoryAsteroides.delete(AnonimeDelete);
+		        	   return AnonimeDelete;
+		           }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Nenhuma informação para ser exibida"));
+ 	}
 }
