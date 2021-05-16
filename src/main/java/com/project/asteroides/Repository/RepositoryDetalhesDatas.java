d1e5528be061a7f8a61a77ee0008a129bcedc0e2
Mario23junior <mariojunior3251@gmail.com>
2021-05-16 10:02:38 -0300

    Criando metodo de listagem em DetalhesData de exibir asteroides de uma classe especifica

--- src/main/java/com/project/asteroides/Repository/RepositoryDetalhesDatas.java
+++ src/main/java/com/project/asteroides/Repository/RepositoryDetalhesDatas.java
@@ -1,9 +1,12 @@
 package com.project.asteroides.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.asteroides.Model.DetalhesDatas;
 
 public interface RepositoryDetalhesDatas extends JpaRepository<DetalhesDatas, Long>{
-
+  
+	List<DetalhesDatas> findByClasseIgnoreCaseContaining(String classe);
 }
