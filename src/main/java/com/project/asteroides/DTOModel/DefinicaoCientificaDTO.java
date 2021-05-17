1756975204600e4f0b1b57bd3cce8ed8d3546809
Mario23junior <mariojunior3251@gmail.com>
2021-05-17 03:20:25 -0300

    Criando camada DTO das entidades e inserindo o relacionamentos entre elas

---
+++ src/main/java/com/project/asteroides/DTOModel/DefinicaoCientificaDTO.java
@@ -0,0 +1,45 @@
+package com.project.asteroides.DTOModel;
+
+public class DefinicaoCientificaDTO {
+    
+  	private Long id;	
+	private Double diametroMedio;
+	private Double gravidade;
+	private Double distanciaMediadoSol;
+	private Double raio;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Double getDiametroMedio() {
+		return diametroMedio;
+	}
+	public void setDiametroMedio(Double diametroMedio) {
+		this.diametroMedio = diametroMedio;
+	}
+	public Double getGravidade() {
+		return gravidade;
+	}
+	public void setGravidade(Double gravidade) {
+		this.gravidade = gravidade;
+	}
+	public Double getDistanciaMediadoSol() {
+		return distanciaMediadoSol;
+	}
+	public void setDistanciaMediadoSol(Double distanciaMediadoSol) {
+		this.distanciaMediadoSol = distanciaMediadoSol;
+	}
+	public Double getRaio() {
+		return raio;
+	}
+	public void setRaio(Double raio) {
+		this.raio = raio;
+	}
+	
+	
+	
+	
+}
