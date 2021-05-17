987c6f6bcb6889d534ff2ef7d89408dbd91f49f1
Mario23junior <mariojunior3251@gmail.com>
2021-05-17 03:40:01 -0300

    Criando a camada de configuracao do ModelMapper

---
+++ src/main/java/com/project/asteroides/Config/ConfigModelMapper.java
@@ -0,0 +1,15 @@
+package com.project.asteroides.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigModelMapper {
+    
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
+	}
+}
